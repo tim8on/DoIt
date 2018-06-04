d1f9d315c7dccea218b1db071a6402cdfa5f88b1
tim8on <tim8on@gmail.com>
2018-06-04 14:33:06 -0700

    add task UI

---
+++ DoIt/CreateTaskViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CreateTaskViewController.swift
+//  DoIt
+//
+//  Created by Tim Aton on 6/4/18.
+//  Copyright © 2018 Tim Aton. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var taskNameTextField: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+
+}
